fe23e40d554734493f83f0fe1cf762848896cd15
Baga9898 <61385284+Baga9898@users.noreply.github.com>
2022-11-05 16:18:10 +0300

    Sidebar is ready

--- src/components/sidebarMenu/sidebarMenu.jsx
+++ src/components/sidebarMenu/sidebarMenu.jsx
@@ -15,13 +15,17 @@ const SidebarMenu = () => {
         <button style={{position: 'absolute', left: '50%'}} onClick={openCloseSidebar}>open/close</button>
         <div className={sidebarIsOpen? 'sidebar active' : 'sidebar'}>
             {sidebarMenuItems.map(item => (
-                <div key={item.section}>
+                <div className='sidebar__section' key={item.section}>
                     <h3>{item.section}</h3>
                     <ul>
                         {item.items.map(item => (
-                            <Link key={item.path} to={item.path}>{item.title}</Link>
+                            <Link key={item.path} to={item.path} className='sidebar__item'>
+                                <div>{item.icon}</div>
+                                <p>{item.title}</p>
+                            </Link>
                         ))}
                     </ul>
+                    <div className='sidebar__devider'></div>
                 </div>
             ))}
         </div>
--- src/components/sidebarMenu/sidebarMenuItems.js
+++ src/components/sidebarMenu/sidebarMenuItems.js
@@ -1,22 +1,31 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { 
+    faWarehouse, 
+    faUsersGear, 
+    faChartSimple, 
+    faBox, 
+    faUserTag 
+} from '@fortawesome/free-solid-svg-icons';
+
 export const sidebarMenuItems = [
     {
         section: 'Main',
         items: [
-            { title: 'warehouses', path: '/warehouses', icon: '' },
+            { title: 'warehouses', path: '/warehouses', icon: <FontAwesomeIcon icon={faWarehouse} /> },
         ]
     },
     {
         section: 'Statistic',
         items: [
-            { title: 'statistic', path: '/statistic', icon: '' },
-            { title: 'leftovers', path: '/leftovers', icon: '' },
+            { title: 'statistic', path: '/statistic', icon: <FontAwesomeIcon icon={faChartSimple} /> },
+            { title: 'leftovers', path: '/leftovers', icon: <FontAwesomeIcon icon={faBox} /> },
         ]
     },
     {
         section: 'Admin',
         items: [
-            { title: 'users', path: '/users', icon: '' },
-            { title: 'roles', path: '/roles', icon: '' },
+            { title: 'users', path: '/users', icon: <FontAwesomeIcon icon={faUsersGear} /> },
+            { title: 'roles', path: '/roles', icon: <FontAwesomeIcon icon={faUserTag} /> },
         ]
     },
 ]
