68444787643d02b030b382c7a3340ff842180a68
Baga9898 <61385284+Baga9898@users.noreply.github.com>
2022-11-06 11:39:46 +0300

    Add warehouses list

--- src/components/pages/warehouses/warehouses.jsx
+++ src/components/pages/warehouses/warehouses.jsx
@@ -1,8 +1,33 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faWarehouse } from '@fortawesome/free-solid-svg-icons';
+
+import './warehouses.scss';
+
+const mockWarehouses = [
+  { name: 'WH-1' },
+  { name: 'WH-2' },
+  { name: 'WH-3' },
+  { name: 'WH-4' },
+  { name: 'WH-5' },
+  { name: 'WH-6' },
+  { name: 'WH-7' },
+  { name: 'WH-8' },
+]
 
 const Warehouses = () => {
   return (
-    <h1>Warehouses</h1>
+    <section className='warehouses'>
+      <h1>Warehouses</h1>
+      <div className='warehouses__list'>
+        {mockWarehouses.map(warehouse => (
+          <div className='warehouses__item'>
+            <FontAwesomeIcon icon={faWarehouse} />
+            <p>{warehouse.name}</p>
+          </div>
+        ))}
+      </div>
+    </section>
   )
 }
 
