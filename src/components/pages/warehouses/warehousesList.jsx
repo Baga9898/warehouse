3670c364841bd294ab6de84e7c2726d534d1dacd
Baga9898 <61385284+Baga9898@users.noreply.github.com>
2022-11-13 11:37:13 +0300

    Add warehouse items stylization

--- src/components/pages/warehouses/warehousesList.jsx
+++ src/components/pages/warehouses/warehousesList.jsx
@@ -1,4 +1,8 @@
-import { faWarehouse }       from '@fortawesome/free-solid-svg-icons';
+import { 
+  faPencil,
+  faTrash,
+  faWarehouse, 
+}                            from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon }   from '@fortawesome/react-fontawesome';
 import React                 from 'react';
 
@@ -6,9 +10,21 @@ const WarehousesList = ({ warehouses }) => {
   return (
     <div className='warehouses__list'>
         {warehouses.map(warehouse => (
-          <div key={warehouse.name} className='warehouses__item'>
-            <FontAwesomeIcon icon={faWarehouse} />
-            <p>{warehouse.name}</p>
+          <div key={warehouse.name} className='warehouses__item' style={{backgroundImage: `url(${warehouse.image})`}}>
+            <div className={'warehouses__item-overlay ' + (warehouse.leftovers > warehouse.capacity
+                ? 'a-lot-of' : '')
+              }
+            >
+              <div className='warehouses__item-actions'>
+                <FontAwesomeIcon icon={faPencil} />
+                <FontAwesomeIcon icon={faTrash} />
+              </div>
+              <FontAwesomeIcon icon={faWarehouse} />
+              <p>{warehouse.name}</p>
+              <span className='warehouses__item-leftovers'>
+                {`${warehouse.leftovers} / ${warehouse.capacity}`}
+              </span>
+            </div>
           </div>
         ))}
     </div>
