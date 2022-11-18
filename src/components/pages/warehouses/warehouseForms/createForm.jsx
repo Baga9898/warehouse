e82d6db4e381ccc0d5189e034b4fb367f0685699
Baga9898 <61385284+Baga9898@users.noreply.github.com>
2022-11-19 00:14:32 +0300

    Rewrite stylization for create window

--- src/components/pages/warehouses/warehouseForms/createForm.jsx
+++ src/components/pages/warehouses/warehouseForms/createForm.jsx
@@ -5,6 +5,8 @@ import React, { useState }   from 'react';
 import { addWarehouse }      from './../../../../api/requests/warehouses';
 import FormInput             from '../../../shared/formInput/formInput';
 
+import './createForm.scss';
+
 const CreateForm = ({ setIsCreateModalOpen }) => {
     const dispatch = useDispatch();
     const [warehouseForm, setWarehouseForm] = useState({
@@ -24,38 +26,40 @@ const CreateForm = ({ setIsCreateModalOpen }) => {
     }
 
   return (
-    <div className='createWarehouse__form'>
-        <FormInput 
-            label='Name' 
-            placeholder='Warehouse name' 
-            changeFunction={(e) => setWarehouseForm({ ...warehouseForm, name: e.target.value })}
-            value={warehouseForm.name} 
-        />
-        <FormInput 
-            label='Columns' 
-            placeholder='Number of columns' 
-            changeFunction={(e) => setWarehouseForm({ ...warehouseForm, col: e.target.value })} 
-            value={warehouseForm.col}
-            icon={faCircleInfo}
-        />
-        <FormInput 
-            label='Rows' 
-            placeholder='Number of rows'  
-            changeFunction={(e) => setWarehouseForm({ ...warehouseForm, row: e.target.value })}  
-            value={warehouseForm.row}
-            icon={faCircleInfo} 
-        />
-        <FormInput 
-            label='Adress' 
-            placeholder='Adress' 
-            changeFunction={(e) => setWarehouseForm({ ...warehouseForm, adress: e.target.value })} 
-            value={warehouseForm.adress}
-        />
+    <>
+        <div className='createWarehouse__form'>
+            <FormInput 
+                label='Name' 
+                placeholder='Warehouse name' 
+                changeFunction={(e) => setWarehouseForm({ ...warehouseForm, name: e.target.value })}
+                value={warehouseForm.name} 
+            />
+            <FormInput 
+                label='Columns' 
+                placeholder='Number of columns' 
+                changeFunction={(e) => setWarehouseForm({ ...warehouseForm, col: e.target.value })} 
+                value={warehouseForm.col}
+                icon={faCircleInfo}
+            />
+            <FormInput 
+                label='Rows' 
+                placeholder='Number of rows'  
+                changeFunction={(e) => setWarehouseForm({ ...warehouseForm, row: e.target.value })}  
+                value={warehouseForm.row}
+                icon={faCircleInfo} 
+            />
+            <FormInput 
+                label='Adress' 
+                placeholder='Adress' 
+                changeFunction={(e) => setWarehouseForm({ ...warehouseForm, adress: e.target.value })} 
+                value={warehouseForm.adress}
+            />
+        </div>
         <div className='createWarehouse__footer'>
             <button onClick={() => setIsCreateModalOpen(false)}>Cancel</button>
             <button onClick={createWarehouse}>Create</button>
         </div>
-    </div>
+    </>
   )
 }
 
