c3149e1eb20e1b2bfc4a1f27cb84ba849daea00a
Baga9898 <61385284+Baga9898@users.noreply.github.com>
2022-12-08 23:03:30 +0300

    Add paddings for modal

--- src/components/pages/rack/rack.jsx
+++ src/components/pages/rack/rack.jsx
@@ -5,14 +5,24 @@ import './rack.scss';
 const Rack = ({ rack }) => {
     return (
         rack ? (
-            <div>
-                <p>{rack.cell}</p>
-                <p>{rack.section}</p>
-                <p>{rack.category}</p>
-                <p>{`${rack.capacity}/${rack.leftovers}`}</p>
-            </div>
+            <>
+                <>
+                    <button>Info</button>
+                    <div className='rackModal__body'>
+                        <p><span>Name: </span>{rack.name}</p>
+                        <p><span>Section: </span>{rack.section}</p>
+                        <p><span>Category: </span>{rack.category}</p>
+                        <p><span>Leftovers: </span>{`${rack.leftovers}/${rack.capacity}`}</p>
+                    </div>
+                </>
+                <div>
+                    {rack.shelves.map((shelve) => shelve)}
+                </div>
+            </>
         ) : (
-            <div>Create Rack</div>
+            <>
+                <div className='rackModal__body'>Create Rack</div>
+            </>
         )
     );
 }
