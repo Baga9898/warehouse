3dc5a820af0f1e64fa26a0e6a169de0fab54b43c
Baga9898 <61385284+Baga9898@users.noreply.github.com>
2022-12-10 22:46:16 +0300

    Add input for create shelve form

--- src/components/pages/rack/rack.jsx
+++ src/components/pages/rack/rack.jsx
@@ -2,15 +2,21 @@ import { faAngleUp }         from '@fortawesome/free-solid-svg-icons';
 import { faCircleInfo }      from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon }   from '@fortawesome/react-fontawesome';
 import React, { useState }   from 'react';
+import FormInput from '../../shared/formInput/formInput';
 
 import './rack.scss';
 
 const Rack = ({ rack }) => {
     const [isAdvancedMenuOpen, setIsAdvancedMenuOpen] = useState(false);
+    const [shelveForm, setShelveForm] = useState({name: ''});
 
     const switchAdvancedMenu = () => {
         setIsAdvancedMenuOpen(!isAdvancedMenuOpen);
     };
+
+    const changeShalveName = () => {
+        setShelveForm
+    }
 
     return (
         rack ? (
@@ -28,12 +34,18 @@ const Rack = ({ rack }) => {
                 </div>
                 <div className='rackModal__shelves'>
                     {rack.shelves.map((shelve) => (
-                        <div className='shelve'>{shelve}</div>
+                        <div key={shelve} className='shelve'>{shelve}</div>
                     )) }
                 </div>
             </div>
         ) : (
-            <div className='rackModal__body'>Create rack form here</div>
+            <div className='rackModal__body createShelve__form'>
+                <FormInput 
+                    label='name'
+                    placeholder='shelve name'
+                    changeFunction={setShelveForm}
+                />
+            </div>
         )
     );
 }
