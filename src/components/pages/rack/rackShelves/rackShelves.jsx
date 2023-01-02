11b0f66bd5d702518625ed3ee32f1712b369a869
Baga9898 <61385284+Baga9898@users.noreply.github.com>
2023-01-02 13:44:40 +0300

    Add shelve actions

--- src/components/pages/rack/rackShelves/rackShelves.jsx
+++ src/components/pages/rack/rackShelves/rackShelves.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import { faPencil, faTrash }   from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon }     from '@fortawesome/react-fontawesome';
+import React                   from 'react';
 
 const RackShelves = ({ currentRackShelves }) => {
     return (
         <div className='rackModal__shelves'>
             {currentRackShelves.length !== 0 ? (
                 currentRackShelves.map((shelve) => (
-                    <div key={shelve.shelve} className='shelve'>{shelve.shelve}</div>
+                    <div key={shelve.shelve} className='shelve'>
+                        {shelve.shelve}
+                        <div className='shelve__actions'>
+                            <FontAwesomeIcon icon={faPencil} />
+                            <FontAwesomeIcon icon={faTrash} />
+                        </div>
+                    </div>
             ))) : <p>oops, nothing seems to be<br/>here yet</p> }
         </div>
     );
