6b35275cd7de76ca715500f2570c14a188adcb84
Baga9898 <61385284+Baga9898@users.noreply.github.com>
2023-01-22 01:06:29 +0300

    fix(server.js): Add dirname for static path

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.static(__dirname));
-app.use(express.static(path.resolve(__, 'build')));
+app.use(express.static(path.resolve(__dirname, 'build')));
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
