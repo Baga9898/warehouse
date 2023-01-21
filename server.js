5c4c0ca913f90aeb3554da0f2c0ce4ebf1deba9e
Baga9898 <61385284+Baga9898@users.noreply.github.com>
2023-01-22 01:11:48 +0300

    fix(server.js): Rewrite distination path

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.static(__dirname));
-app.use(express.static(path.resolve(__dirname, 'build')));
+app.use(express.static(path.resolve(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 app.listen(PORT);
